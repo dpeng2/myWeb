cbaab374333eeaee78e88c6774b9de041f0b54b8
dpeng2 <786798146@qq.com>
2020-10-20 22:31:51 +0800

    添加后台模块

--- src/com/mingrisoft/News.java
+++ src/com/mingrisoft/News.java
@@ -3,6 +3,8 @@ package com.mingrisoft;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class News {
 	DBConnection DBConn = new DBConnection();                      // 引入连接数据库方法类
@@ -19,7 +21,7 @@ public class News {
 			int intPage = 1;                                      // 定义数字型变量并赋值1
 			int intPageSize = 5;                                 // 定义数字型变量并赋值5
 			// 创建sql语句查询News表全部信息
-			String sSql = "select * from news";
+			String sSql = "select * from news order by NewsID desc";
 			// 通过执行sql语句得到查询结果
 			rs = stmt.executeQuery(sSql);	
 			if (!rs.next()) {                                     // 判定当查询结果为空
@@ -85,7 +87,7 @@ public class News {
 	public String FrontNewsDetail(String s0) {
 		try {// 捕获系统异常
 			Connection Conn = DBConn.getConn();                      // 建立数据库连接
-			Statement stmt = Conn.createStatement();                 // 创建数据查询
+			Statement stmt = Conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);                 // 创建数据查询
 			ResultSet rs = null;                                     // 定义结果集
 			int NewsID = Fun.StrToInt(s0);                           // 将参数s0进行转换
 			if (NewsID == 0) {                                       // s0判定等于0
@@ -131,6 +133,235 @@ public class News {
 			return "No";                                              // 系统异常后返回字符“No”
 		}
 	}
+	
+	 public String ListNews(String sPage,String strPage)
+	    {			
+			try
+	    	{
+				
+	    		Connection Conn = DBConn.getConn();
+		    	Statement stmt = Conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+		    	ResultSet rs = null;
+	    		StringBuffer sb = new StringBuffer();
+	    		
+				int i;
+				int intPage = 1; 			
+				int intPageSize = 5;
+				
+		    	String sSql = "select * from News order by NewsID desc";
+		    	rs = stmt.executeQuery(sSql);
+		    	
+	    		
+		    	
+				if (!rs.next())
+				{ 
+					sb.append("<tr height=\"25\" bgcolor=\"#d6dff7\"  class=\"info1\"><td colspan=\"4\">\r\n");
+					sb.append("<div align=\"center\"><b>没有记录!</b></div></td></tr>\r\n");
+				}
+				else 
+				{
+					
+					intPage = Fun.StrToInt(strPage);
+					sPage = Fun.CheckReplace(sPage);
+					if (intPage==0) intPage=1;	
+		
+					String NewsTitle2 = rs.getString("NewsTitle");
+					// 定义数字型变量并赋值News表里的NewsTime属性
+					String NewsTime2 = rs.getString("NewsTime");
+					// 定义数字型变量并赋值News表里的AdminName属性
+					String AdminName2 = rs.getString("AdminName");
+					
+					rs.absolute((intPage-1) * intPageSize+1);
+					i = 0; 
+					while(i < intPageSize && !rs.isAfterLast())
+					{		
+					    int NewsID = rs.getInt("NewsID");	
+						String NewsTitle = rs.getString("NewsTitle");
+						String NewsContent = rs.getString("NewsContent");
+						String NewsTime = rs.getString("NewsTime");
+						String AdminName = rs.getString("AdminName");
+						
+						
+						sb.append("<tr>");
+						sb.append("<td class=\"table-id\">"+NewsID+"</td>");
+						
+						sb.append("<td>"+NewsTitle+"</td>");
+						sb.append("<td class=\"table-title\">"+AdminName+"</td>");
+						sb.append("<td class=\"table-title\">"+NewsTime+"</td>");
+						sb.append("<td><div class=\"am-btn-toolbar\">");
+						sb.append("<div class=\"am-btn-group am-btn-group-xs\">");
+						sb.append("<input type=\"hidden\" value=\""+NewsID+"\">");
+						sb.append("<input type=\"hidden\" value=\""+NewsContent+"\">");
+						sb.append("<input type=\"hidden\" value=\""+NewsTitle+"\">");
+						sb.append("<a onclick=\"edit(this);\"");
+						sb.append("class=\"am-btn am-btn-primary am-btn-xs \"");		
+						sb.append("href=\"javascript:void(0);\"> <span></span> 修改 <a> ");			
+						sb.append("<a rel=\""+NewsID+"\" onclick=\"del(this);\" class=\"am-btn am-btn-danger am-btn-xs \" href=\"javascript:void(0);\"> " +
+								"<span></span>删除<a>");			
+						sb.append("</div></div></td></tr>");		
+								
+						
+						rs.next(); 
+						i++;
+					}
+					sb.append(Fun.Page(sPage,rs,intPage,intPageSize));
+				}
+				rs.close();
+	    		stmt.close();
+	    		Conn.close();
+	    		return sb.toString();	
+	    	}catch(Exception e)
+	        {
+	            return "No";
+	        }
+	    }
+	    
+	    
+	    
+	  
+	    public String AddNews(String [] s,String s1,String s2)
+	    {			
+			try
+	    	{
+	    		Connection Conn = DBConn.getConn();
+		    	Statement stmt = Conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+	    		
+		    	ResultSet rs = null;
+		    	String sSql = "select * from News order by NewsID desc";   
+		    	rs = stmt.executeQuery(sSql);
+		    	int z=0;
+		    	int newNum=0;
+		    	if (!rs.next())
+				{ 
+					newNum=1;
+				}else{
+					while(z <1  && !rs.isAfterLast())
+					{		
+					    int NewsID = rs.getInt("NewsID");
+					    newNum=NewsID+1;
+						break;
+					}
+				}
+		    	
+		    	for(int i=0;i<s.length;i++)
+		    	{
+		    		if(i!=1) s[i] = Fun.getStrCN(Fun.CheckReplace(s[i]));
+		    		else s[i] = Fun.getStrCN(s[i]);
+		    	}
+		    	
+		    	    SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+			    	String newsTime =  format1.format(new Date());
+
+			    	StringBuffer sql= new StringBuffer();
+			    	sql.append("insert into News (NewsID,NewsTitle,NewsContent,NewsTime,AdminName) values (" +
+			    			" '"+newNum+"',"+
+			    			" '"+s[0]+"',"+
+			    			" '"+s[1]+"'," +
+			    			" '"+newsTime+"'," +
+			    			" '"+s1+"')");
+			    	
+					System.out.println(sql);
+					
+					try{
+						
+						Conn.setAutoCommit(false);
+						stmt.execute(sql.toString());
+						Conn.commit();
+						Conn.setAutoCommit(true);
+						stmt.close();
+						Conn.close();
+						
+				    	return "Yes";
+				    }catch (Exception e) {
+							Conn.rollback();			
+							e.printStackTrace();
+							Conn.close();
+							return "添加成功!";
+							}  
+		    }catch(Exception e)
+	        {
+	            e.printStackTrace();
+	            return "添加失败";
+	        }
+	     }
+	    
+	    
+	    
+	    
+	    public String EditNews(String [] s,String s0,String s1,String s2)
+	    {			
+			try
+	    	{
+
+	    		Connection Conn = DBConn.getConn();
+		    	Statement stmt = Conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+	    		
+		    	for(int i=0;i<s.length;i++)
+		    	{
+		    		s[i] = Fun.getStrCN(Fun.CheckReplace(s[i]));
+		    	}
+		    	
+		    	int NewsID = Fun.StrToInt(s0);
+		    			
+		    	StringBuffer sql = new StringBuffer();
+		    	sql.append("update News set NewsTitle='"+s[0]+"'" +
+		    			   " ,NewsContent='"+s[1]+"'" +
+		    			   " where NewsID='"+NewsID+"'");
+				
+				stmt.executeUpdate(sql.toString());
+				stmt.close();
+				Conn.close();
+
+				return "Yes";
+			    
+		    }catch(Exception e)
+	        {
+	            e.printStackTrace();
+	            System.out.print(e.getMessage());
+	            return "编辑错误!";
+	        }
+	     }
+	    
+	    
+	    
+	    public boolean DelNews(String s0,String s1,String s2)
+	    {
+	    	
+	    	try{
+		    	Connection Conn = DBConn.getConn();
+		    	Statement stmt = Conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+		    	int NewsID = Fun.StrToInt(s0);
+		    	if (NewsID==0)
+		    		return false;
+		    	else
+		    	{
+				  try{
+				    	String sql = "delete from News where NewsID=" + NewsID;
+				    	
+				    	Conn.setAutoCommit(false);
+				    	stmt.executeUpdate(sql);
+			
+				    	Conn.commit();
+				    	Conn.setAutoCommit(true);
+				    	
+						stmt.close();
+						Conn.close();
+				    	return true;
+			    	}catch (Exception e) {
+							Conn.rollback();		
+							//e.printStackTrace();
+							Conn.close();
+							return false;
+							}  
+			    }
+	    	}catch(Exception e){
+	    		//e.printStackTrace();
+	    		//System.out.print(e.getMessage());
+	    		
+	    		return false;
+	    	}
+		
+		}
 
 
 }
